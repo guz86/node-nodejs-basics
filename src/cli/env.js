c6b13b5d9e8dfaee6a05ab0306294a242fd6cdd7
Pavel Gordienko <guzmo@yandex.ru>
2024-10-08 00:57:34 +0300

    feat: add env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVars = process.env;
+
+    const rssVars = Object.entries(envVars)
+        .filter(([key]) => key.startsWith('RSS_'))
+        .map(([key, value]) => `${key}=${value}`);
+
+    if (rssVars.length === 0) {
+        console.log('No found');
+        return;
+    }
+
+    const output = rssVars.join('; ');
+
+    console.log(output);
 };
 
 parseEnv();
